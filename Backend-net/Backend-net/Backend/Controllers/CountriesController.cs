a3d4fc29eba3651c36db603360fb4d36dd314bff
szuluaga-beep <steven000069@hotmail.com>
2024-11-20 15:37:24 -0500

    Creating project backend

---
+++ Backend-net/Backend-net/Backend/Controllers/CountriesController.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class CountriesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public CountriesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        var countries = await _context.Countries.ToListAsync();
+        return Ok(countries);
+    }
+}
